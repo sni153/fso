8f95773c6f7e4ba5219d7a248f313029a606f5da
Steven Ni <steven.christopher.ni@gmail.com>
2023-10-30 22:54:25 -0600

    update 1.1: course information solution to include Total

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -18,6 +18,10 @@ const Content = (props) => {
   );
 };
 
+const Total = (props) => {
+  return <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>;
+};
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -36,6 +40,11 @@ const App = () => {
         part2={part2}
         exercises2={exercises2}
         part3={part3}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
         exercises3={exercises3}
       />
     </div>
