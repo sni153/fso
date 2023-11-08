6aa7bf1819bc89afc29ba67bb5170ba230fa2b98
Steven Ni <steven.christopher.ni@gmail.com>
2023-11-08 12:35:17 -0700

    add solution for add solution for 1.6: unicafe step1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,4 +1,18 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
+import "./App.css";
+
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
+
+const Statistic = (props) => {
+  return (
+    <div>
+      {props.text} {props.count}
+    </div>
+  );
+};
 
 const App = () => {
   // save clicks of each button to its own state
@@ -6,7 +20,31 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
+  return (
+    <>
+      <h3>give feedback</h3>
+      <div className="button-container">
+        <Button text="good" handleClick={handleGoodClick} />
+        <Button text="neutral" handleClick={handleNeutralClick} />
+        <Button text="bad" handleClick={handleBadClick} />
+      </div>
+      <h3>statistics</h3>
+      <div>
+        <Statistic text="good" count={good} />
+        <Statistic text="neutral" count={neutral} />
+        <Statistic text="bad" count={bad} />
+      </div>
+    </>
+  );
 };
 
 export default App;
