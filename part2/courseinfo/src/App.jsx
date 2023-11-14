28bb73eaab5d6c94102106416ac0e8ad630de713
Steven Ni <steven.christopher.ni@gmail.com>
2023-11-13 17:27:43 -0700

    add solution for 2.2: Course information step7

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -7,8 +7,23 @@ const Part = ({ part }) => (
   </div>
 );
 
-const Content = ({ parts }) =>
-  parts.map((part) => <Part key={part.id} part={part} />);
+const Content = ({ parts }) => {
+  const sum = parts.reduce(
+    (accum, currentValue) => accum + currentValue.exercises,
+    0
+  );
+
+  return (
+    <>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+      <p><b>total of {sum} exercises</b></p>
+    </>
+  );
+};
+
+const sum = () => {};
 
 const Course = ({ course }) => (
   <>
