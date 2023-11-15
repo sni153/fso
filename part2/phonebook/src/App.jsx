d7d8901008c7c5a8d94e98bc6152ea87fb48ddeb
Steven Ni <steven.christopher.ni@gmail.com>
2023-11-14 23:55:52 -0700

    refactor solution to 2.9*: The Phonebook Step4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,16 +2,21 @@
 import { useState } from "react";
 
 const App = () => {
+  // State variables
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-123456", id: 1 },
     { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
     { name: "Dan Abramov", number: "12-43-234345", id: 3 },
     { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [search, setSearch] = useState("");
 
+  const [formData, setFormData] = useState({
+    newName: "",
+    newNumber: "",
+    search: "",
+  });
+
+  // Component-specific variables
   const Person = ({ person }) => {
     return (
       <p>
@@ -20,38 +25,39 @@ const App = () => {
     );
   };
 
-  const addName = (event) => {
-    event.preventDefault();
-    const newPerson = {
-      name: newName,
-      number: newNumber,
-    };
-    const names = persons.map((person) => person.name.toLowerCase());
-    if (names.includes(newName.toLowerCase())) {
-      alert(`${newName} is already added to phonebook`);
-    } else {
-      setPersons(persons.concat(newPerson));
-      setNewName("");
-      setNewNumber("");
-    }
-  };
-
+  // Event handlers
   const handleSearch = (event) => {
-    setSearch(event.target.value);
+    setFormData({ ...formData, search: event.target.value });
   };
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value);
+    setFormData({ ...formData, newName: event.target.value });
   };
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
+    setFormData({ ...formData, newNumber: event.target.value });
   };
 
+  const addName = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: formData.newName,
+      number: formData.newNumber,
+    };
+    const names = persons.map((person) => person.name.toLowerCase());
+    if (names.includes(formData.newName.toLowerCase())) {
+      alert(`${formData.newName} is already added to phonebook`);
+    } else {
+      setPersons(persons.concat(newPerson));
+      setFormData({ ...formData, newName: "", newNumber: "" });
+    }
+  };
+
+  // Render function
   const PeopleToDisplay = () => {
-    if (search) {
+    if (formData.search) {
       return persons
-        .filter((person) => person.name.includes(search))
+        .filter((person) => person.name.includes(formData.search))
         .map((person) => <Person key={person.name} person={person} />);
     } else {
       return persons.map((person) => (
@@ -62,19 +68,22 @@ const App = () => {
     }
   };
 
+  // Component JSX
   return (
     <div>
       <h2>Phonebook</h2>
       <div>
-        filter shown with <input value={search} onChange={handleSearch}></input>
+        filter shown with{" "}
+        <input value={formData.search} onChange={handleSearch}></input>
       </div>
       <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
+          name: <input value={formData.newName} onChange={handleNameChange} />
         </div>
         <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
+          number:{" "}
+          <input value={formData.newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
