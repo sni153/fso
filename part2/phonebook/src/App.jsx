22bddd01c0f0a2aa12b27a28dc7f6595af25be2b
Steven Ni <steven.christopher.ni@gmail.com>
2023-11-16 21:51:23 -0700

    add solution for 2.11: The Phonebook Step6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,33 +3,38 @@ import { useState } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import { useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
   // State variables
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [formData, setFormData] = useState({
     newName: "",
     newNumber: "",
     search: "",
   });
 
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
+
   // Event handlers
   const handleSearch = (event) => {
-    setFormData({ ...formData, search: event.target.value });
+    const search = event.target.value;
+    setFormData((prevData) => ({...prevData, search}));
   };
 
   const handleNameChange = (event) => {
-    setFormData({ ...formData, newName: event.target.value });
+    const newName = event.target.value;
+    setFormData((prevData) => ({ ...prevData, newName }));
   };
 
   const handleNumberChange = (event) => {
-    setFormData({ ...formData, newNumber: event.target.value });
+    const newNumber = event.target.value;
+    setFormData((prevData) => ({ ...prevData, newNumber }));
   };
 
   const addName = (event) => {
