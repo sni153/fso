8535903ac5783770b28a4208a44ead13eae1bded
Steven Ni <steven.christopher.ni@gmail.com>
2023-11-25 14:33:49 -0700

    add solution for 3.1: Phonebook backend step1

---
+++ part3/backend-example/index.js
@@ -0,0 +1,88 @@
+// Import the express module and create an express application
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// Array of notes
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+// Define a route for the root path ("/")
+app.get("/", (request, response) => {
+  // Send a response with the string "Hello World!"
+  response.send("<h1>Hello World!</h1>");
+  // Express automatically sets the Content-Type header to "text/html" for the response.
+});
+
+// Define a route for the "/api/notes" path
+app.get("/api/notes", (request, response) => {
+  // Send the "notes" array as a JSON response
+  response.json(notes);
+  // Express automatically sets the Content-Type header to "application/json" for the response.
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+const PORT = 3001;
+
+// Start the server and listen for HTTP requests on port 3001
+app.listen(PORT, () => {
+  // Log a message indicating the server is running
+  console.log(`Server running on port ${PORT}`);
+});
