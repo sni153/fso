33412bf23666d5992cf12e84a752225f93bb59bd
Steven Ni <steven.christopher.ni@gmail.com>
2023-11-30 20:59:48 -0700

    add solution for 3.12: Command-line database

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.mnjdgyu.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: name,
+  number: number,
+});
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+if (process.argv.length < 4) {
+  console.log("phonebook:");
+  Contact.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person.name, person.number);
+    });
+    mongoose.connection.close();
+  });
+} else if (process.argv.length === 5) {
+  const person = new Person({
+    name: name,
+    number: number,
+  });
+  person.save().then(() => {
+    console.log(`Added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else if (process.argv.length > 5) {
+  console.log('Name needs to be enclosed in quotes: node mongo.js <password> "<first></last>" <number>');
+  console.log(process.argv)
+  mongoose.connection.close();
+}
