da86c36900db0f85fa81d54adcce46e3c956af5a
Steven Ni <steven.christopher.ni@gmail.com>
2023-12-02 23:18:48 -0700

    add solution for 3.17*: Phonebook database, step5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -134,6 +134,18 @@ app.post("/api/persons", (req, res) => {
     .catch((error) => next(error));
 });
 
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => res.json(updatedPerson))
+    .catch((error) => next(error));
+});
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
