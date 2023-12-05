215302dfeaaea2d85d615b4218072dad5e0b12b4
Steven Ni <steven.christopher.ni@gmail.com>
2023-12-04 19:21:45 -0700

    update blog app with jest and sample tests

---
+++ part4/blog/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'commonjs': true,
+		'es2021': true,
+		'node': true,
+		'jest':true,
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 'latest'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	}
+}
