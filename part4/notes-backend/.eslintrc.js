64c73c91db85055d83c3f742f129c001bfbfc0c6
Steven Ni <steven.christopher.ni@gmail.com>
2023-12-05 13:16:28 -0700

    add notes backend and frontend to continue testing examples

---
+++ part4/notes-backend/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'linebreak-style': ['error', 'unix'],
+    'no-console': 0,
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+  },
+}
