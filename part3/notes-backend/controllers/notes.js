8af53a0e81fc89b9500e9228b7c2746596d459fd
Steven Ni <steven.christopher.ni@gmail.com>
2023-12-07 14:26:54 -0700

    update put request to use async/await syntax

--- part3/notes-backend/controllers/notes.js
+++ part3/notes-backend/controllers/notes.js
@@ -30,19 +30,14 @@ notesRouter.delete('/:id', async (request, response) => {
   response.status(204).end()
 })
 
-notesRouter.put('/:id', (request, response, next) => {
+notesRouter.put('/:id', async (request, response) => {
   const body = request.body
-
   const note = {
     content: body.content,
     important: body.important,
   }
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then(updatedNote => {
-      response.json(updatedNote)
-    })
-    .catch(error => next(error))
+  const updatedNote = Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    response.json(updatedNote)
 })
 
 module.exports = notesRouter
