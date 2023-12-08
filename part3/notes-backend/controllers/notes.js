ad4a28d265050e086e94be19cfcc29a69f028c7f
Steven Ni <steven.christopher.ni@gmail.com>
2023-12-08 16:02:50 -0700

    add the use of token-based authentication

--- part3/notes-backend/controllers/notes.js
+++ part3/notes-backend/controllers/notes.js
@@ -1,6 +1,21 @@
+const jwt = require('jsonwebtoken')
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  // Extract the 'authorization' header from the request
+  const authorization = request.get('authorization')
+
+  // Check if the authorization header exists and starts with 'Bearer '
+  if (authorization && authorization.startsWith('Bearer ')) {
+    // Return the token by removing the 'Bearer ' prefix
+    return authorization.replace('Bearer ', '')
+  }
+
+  // Return null if no token is found or the authorization header is missing
+  return null
+}
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note
@@ -20,8 +35,12 @@ notesRouter.get('/:id', async (request, response) => {
 
 notesRouter.post('/', async (request, response) => {
   const body = request.body
-
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  console.log(decodedToken)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const note = new Note({
     content: body.content,
