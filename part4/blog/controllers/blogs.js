bb0e13718c33f199243b4e5c1b19c265b5804d1c
Steven Ni <steven.christopher.ni@gmail.com>
2023-12-09 21:02:06 -0700

    add solution for 4.19: bloglist expansion, step7

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,6 +1,16 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+	const authorization = request.get('authorization')
+	if (authorization && authorization.startsWith('Bearer')) {
+		return authorization.replace('Bearer ', '')
+	}
+	return null
+}
+
 
 blogsRouter.get('/', async (request, response) => {
 	const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
@@ -21,21 +31,22 @@ blogsRouter.post('/', async (request, response) => {
 	if (!title || !url) {
 		return response.status(400).json({ error: 'Title and url are required' })
 	}
-	// Fetch a random user from the database
-	const users = await User.find({})
-	const randomIdx = Math.floor(Math.random()* users.length)
-	const randomUser = users[randomIdx]
+	const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+	if (!decodedToken.id) {
+		return response.status(401).json({ error: 'token invalid' })
+	}
+	const user = await User.findById(decodedToken.id)
 		
 	const blog = new Blog({
 		url: url,
 		title: title,
 		author: author,
-		user: randomUser.id,
+		user: user.id,
 		likes: likes === undefined ? 0 : likes,
 	})
 	const savedBlog = await blog.save()
-	randomUser.blogs = randomUser.blogs.concat(savedBlog._id)
-	await randomUser.save()
+	user.blogs = user.blogs.concat(savedBlog._id)
+	await user.save()
 	response.status(201).json(savedBlog)
 })
 
