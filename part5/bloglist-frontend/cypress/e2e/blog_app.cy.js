41ccb68267575cbef79016eb4878b6d54f04cbeb
Steven Ni <steven.christopher.ni@gmail.com>
2023-12-19 18:43:23 -0700

    add solution for 5.18: bloglist end to end testing, step2

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,12 +1,35 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Steph Curry',
+      username: 'chef',
+      password: 'pikmin'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
+    cy.visit('http://localhost:5173')
   })
 
   it('Login form is shown', function() {
-    cy.visit('http://localhost:5173')
     cy.contains('login')
+    cy.contains('log in to application')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('chef')
+      cy.get('#password').type('pikmin')
+      cy.get('#login-button').click()
+
+      cy.contains('Steph Curry logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('chef')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.contains('wrong username or password')
+    })
   })
 })
-
-
