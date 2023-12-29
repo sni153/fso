ecb347f00f8ed0aab518db4393ad2fdad33c5285
Steven Ni <steven.christopher.ni@gmail.com>
2023-12-29 09:00:22 -0700

    add solution for Exercise 6.20 Implement retrieving anecdotes from the server using React Query.

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,3 +1,6 @@
+import { useQuery } from '@tanstack/react-query'
+import { getAnecdotes } from './requests'
+
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
@@ -7,13 +10,17 @@ const App = () => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
