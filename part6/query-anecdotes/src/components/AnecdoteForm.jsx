acec0f7ea48ad8585bb119b01601bb516638867c
Steven Ni <steven.christopher.ni@gmail.com>
2023-12-29 10:06:45 -0700

    Exercise 6.21 Implement adding new anecdotes to the server using React Query

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,16 +1,29 @@
-const AnecdoteForm = () => {
+import { QueryClient, useMutation, useQueryClient } from '@tanstack/react-query'
+import { createAnecdote } from '../requests'
 
-  const onCreate = (event) => {
+const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+
+  const newAnecdoteMutation = useMutation({ 
+    mutationFn: createAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes']})
+    }
+  })
+
+  const handleSubmit = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
+    if (content.length >= 5) {
+      newAnecdoteMutation.mutate({ content, votes: 0 })
+    }
 }
 
   return (
     <div>
       <h3>create new</h3>
-      <form onSubmit={onCreate}>
+      <form onSubmit={handleSubmit}>
         <input name='anecdote' />
         <button type="submit">create</button>
       </form>
