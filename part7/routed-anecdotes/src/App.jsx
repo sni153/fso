f7096430d0cc95790f0db6a6daf05b233f3f42b6
Steven Ni <steven.christopher.ni@gmail.com>
2023-12-31 10:01:05 -0700

    add solution for 7.5: anecdotes and hooks step2
    Implemented reset button to clear input fields

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -79,6 +79,13 @@ const CreateNew = (props) => {
     navigate('/')
   }
 
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -95,7 +102,8 @@ const CreateNew = (props) => {
           url for more info
           <input {...info} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button type="button" onClick={handleReset}>reset</button>
       </form>
     </div>
   )
