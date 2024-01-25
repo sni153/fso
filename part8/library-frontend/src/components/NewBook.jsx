04936a40e6d4c8317b245b7412a5785139354194
Steven Ni <steven.christopher.ni@gmail.com>
2024-01-25 12:40:59 -0700

    add solution for 8.10: Adding a book
    Implemented feature to add new books in the application.

--- part8/library-frontend/src/components/NewBook.jsx
+++ part8/library-frontend/src/components/NewBook.jsx
@@ -1,20 +1,32 @@
 import { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { ALL_AUTHORS, ALL_BOOKS, ADD_BOOK } from '../queries'
 
-const NewBook = (props) => {
+const NewBook = () => {
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
   const [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
 
-  if (!props.show) {
-    return null
-  }
+  const [ createBook ] = useMutation(ADD_BOOK, {
+    refetchQueries: [ { query: ALL_AUTHORS}, { query: ALL_BOOKS } ],
+    onError: (error) => {
+      console.log(error)
+    }
+  })
 
   const submit = async (event) => {
     event.preventDefault()
 
-    console.log('add book...')
+    createBook({ 
+      variables: { 
+        title: title, 
+        author: author,
+        published: parseInt(published),
+        genres: genres
+      } 
+    });
 
     setTitle('')
     setPublished('')
