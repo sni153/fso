4aeb3c2170b7b52e97e72fa5ea83ef9fe08bcea8
Steven Ni <steven.christopher.ni@gmail.com>
2024-02-05 19:09:42 -0700

    Implemented user login functionality and token-based authentication in the frontend

--- part8/graphql-phonebook-frontend/src/App.jsx
+++ part8/graphql-phonebook-frontend/src/App.jsx
@@ -4,15 +4,25 @@ import { useState } from 'react'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import PhoneForm from './components/PhoneForm'
+import LoginForm from './components/LoginForm'
 import Notify from './components/Notify'
 import { ALL_PERSONS } from './queries'
+import { useApolloClient } from '@apollo/client';
 
 const App = () => {
+  const [token, setToken] = useState(null)
   const result = useQuery(ALL_PERSONS)
   const [errorMessage, setErrorMessage] = useState(null)
+  const client = useApolloClient()
 
   if (result.loading)  {
     return <div>loading...</div>
+  }
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
   }
 
   const notify = (message) => {
@@ -22,13 +32,23 @@ const App = () => {
     }, 10000)
   }
 
+  if (!token) {
+    return (
+      <>
+        <Notify errorMessage={errorMessage} />
+        <LoginForm setToken={setToken} setError={notify} />
+      </>
+    )
+  }
+
   return (
-    <div>
+    <>
       <Notify errorMessage={errorMessage} />
-      <Persons persons = {result.data.allPersons} />
+      <button onClick={logout}>logout</button>
+      <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
-      <PhoneForm  setError={notify} />
-    </div>
+      <PhoneForm setError={notify} />
+    </>
   )
 }
 
